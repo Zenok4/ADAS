3fa5c17603c22f2dee1e8094859de442aa6aeab8
kiet020104 <tuankietdo0201@gmail.com>
2025-09-08 10:29:43 +0700

    Sửa file thư mục, chuyển info vào main và phone vào login

--- src/app/(main)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex flex-col min-h-screen bg-[#0a2a43] text-white">
-      {children}
-    </div>
-  );
-};
-
-export default MainLayout;
